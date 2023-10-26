d4f7d8b7dd34b0e72124fa24e83bfaf82bca61b6
nastar <an1413@hs.ac.kr>
2023-10-26 21:58:55 +0900

    Feat: 최종본

--- js/alarm.js
+++ js/alarm.js
@@ -1,13 +1,12 @@
-document.addEventListener("DOMContentLoaded", function () {
-const $alarmForm = document.querySelector("#alarm-form");
-const $alarmWrapper = document.querySelector(".alarm");
-const alarms = [];
+const $alarmForm = document.querySelector("#alarm-form"); // alarm-form등록 선택자
+const $alarmWrapper = document.querySelector(".alarm"); // alarm현황 선택자
+const alarms = []; // 알람배열
 
   // 로컬 스토리지에서 알람 데이터를 불러와서 초기화합니다.
-const storedAlarms = localStorage.getItem("alarms");
-if (storedAlarms) {
-    alarms.push(...JSON.parse(storedAlarms));
-    renderAlarms();
+const storedAlarms = localStorage.getItem("alarms"); // 로컬스토리지 현황담은 변수
+if (storedAlarms) { // 로컬스토리지에 값이 있으면
+    alarms.push(...JSON.parse(storedAlarms)); // alarms 배열에 넣기
+    renderAlarms(); // 랜더링
 }
 
 $alarmForm.addEventListener("submit", function (e) {
@@ -20,49 +19,41 @@ $alarmForm.addEventListener("submit", function (e) {
     const minute = parseInt(minuteInput.value);
     const second = parseInt(secondInput.value);
 
-    if (isNaN(hour) || isNaN(minute) || isNaN(second)) {
-        alert("올바른 시간을 입력하세요.");
-        return;
-    }
-
+    if (isNaN(hour) || isNaN(minute) || isNaN(second) || hour < 0 || hour >= 24 || minute < 0 || minute >= 60 || second < 0 || second >= 60) {
+    alert("올바른 시간을 입력하세요.");
+    return;
+}
+    
     if (alarms.length >= 3) {
-        alert("최대 3개의 알람만 설정할 수 있습니다.");
+        alert("최대 3개의 알람까지만 설정할 수 있습니다.");
         return;
     }
 
-    const alarmTime = `${hour}:${minute}:${second}`;
+    const alarmTime = `${hour}:${minute}:${second}`; // 알람등록시간
 
-    alarms.push(alarmTime);
+    alarms.push(alarmTime); // 알람현황배열에 저장
 
-    localStorage.setItem("alarms", JSON.stringify(alarms));
-
+    // 로컬스토리지에 저장
+    localStorage.setItem("alarms", JSON.stringify(alarms)); 
     renderAlarms();
 
-    hourInput.value = "00";
-    minuteInput.value = "00";
-    secondInput.value = "00";
+    // input값 초기화
+    hourInput.value = "";
+    minuteInput.value = "";
+    secondInput.value = "";
 });
 
-function renderAlarms() {
-    $alarmWrapper.innerHTML = ""; // 목록 초기화
-
-    for (let i = 0; i < alarms.length; i++) {
-    //   const alarmItemWrapper = document.createElement("div"); // 래퍼 요소 추가
-      const alarmItem = document.createElement("li");
-      const alarmDeleteBtn = document.createElement("button");
+function renderAlarms() {  // 알람 랜더링
+    $alarmWrapper.innerHTML = ""; // 알람현황 초기화
+    for (let i = 0; i < alarms.length; i++) { // 알람 배열의 길이만큼 반복하기
+      const alarmItem = document.createElement("li"); // 리스트 생성
+      const alarmDeleteBtn = document.createElement("button"); // 삭제 버튼생성
       alarmDeleteBtn.innerText = "삭제";
 
-      const [hour, minute, second] = alarms[i].split(":").map(Number);
-
-      if (isNaN(hour) || isNaN(minute) || isNaN(second)) {
-        // 알람 데이터가 올바르지 않은 경우 스킵
-        continue;
-      }
-
-      alarmItem.textContent = `알람: ${hour}시 ${minute}분 ${second}초`;
+      console.log(storedAlarms);
+      alarmItem.textContent = `알람: ${alarms[i]}`;
       $alarmWrapper.appendChild(alarmItem);
       alarmItem.appendChild(alarmDeleteBtn);
-    //   $alarmWrapper.appendChild(alarmItem);
 
       alarmDeleteBtn.addEventListener("click", function () {
         alarms.splice(i, 1);
@@ -72,33 +63,6 @@ function renderAlarms() {
     }
   }
 
-  function checkAlarms() {
-    const now = new Date();
-
-    for (let i = 0; i < alarms.length; i++) {
-      const [hour, minute, second] = alarms[i].split(":").map(Number);
-
-      if (isNaN(hour) || isNaN(minute) || isNaN(second)) {
-        // 알람 데이터가 올바르지 않은 경우 스킵
-        continue;
-      }
-
-      const alarmTime = new Date();
-      alarmTime.setHours(hour);
-      alarmTime.setMinutes(minute);
-      alarmTime.setSeconds(second);
-
-      if (now >= alarmTime) {
-        alert("알람이 울립니다!");
-        alarms.splice(i, 1);
-        localStorage.setItem("alarms", JSON.stringify(alarms));
-        renderAlarms();
-        i--;
-      }
-    }
-  }
-
   setInterval(checkAlarms, 1000);
 
   renderAlarms();
-});
